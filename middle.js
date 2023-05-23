2d7108aa78566f730bf4927a3959a6ed4286af96
Afreeda <amahesan@uoguelph.ca>
2023-05-22 21:12:20 -0400

    Implement a function to return the middle values of an array

---
+++ middle.js
@@ -0,0 +1,52 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1,array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const middle = function(array) {
+  const length = array.length;
+  const middleIndex = Math.floor(length / 2);
+
+  if (length <= 2) {
+    return [];
+  }
+
+  if (length % 2 === 0) {
+    return array.slice(middleIndex - 1, middleIndex + 1);
+  } else {
+    return [array[middleIndex]];
+  }
+};
+
+//Test middle function:
+console.log(middle([1])) // => []
+console.log(middle([1, 2])) // => []
+
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+
+let equal = eqArrays(["my", "favourite", "item"], ["my", "favourite", "item"]);
+
+assertArraysEqual(['favourite'], middle(["my", "favourite", "item", "teddy"]));
+assertArraysEqual(['item'], middle(["my", "item", "teddy"]));
+
+assertArraysEqual(['favourite', 'item'], middle(["my", "favourite", "item", "teddy"]));
