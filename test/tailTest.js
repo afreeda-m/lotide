d504036e5f89c4c471e7afa0ed5747334a1c18e9
Afreeda <amahesan@uoguelph.ca>
2023-06-07 13:38:02 -0400

    Delete a test case for tail function only

--- test/tailTest.js
+++ test/tailTest.js
@@ -6,8 +6,6 @@ tail(words);
 assertEqual(words.length, 3);
 
 
-const result = tail(["Hello", "Lighthouse", "Labs", "bootcamp"]);
-console.log(result);
 assertEqual(result.length, 3);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
