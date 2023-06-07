dd5a72a8ec50030870aa1918116b382a6d79ff6a
Afreeda <amahesan@uoguelph.ca>
2023-06-07 16:11:40 -0400

    Add module.exports = eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -8,27 +8,30 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
+  if (Object.keys(object1).length !== Object.keys(object2).length){
     return false;
   }
 
-  for (let key in object1) {
-    if (object2[key]) {
-      if (object1[key] !== object2[key]) {
+  for (let key in object1){
+    if (object2[key]){
+      if (object1[key] !== object2[key]){
         return false;
       }
     } else {
+      
       return false;
     }
   }
   return true;
 };
 
+module.exports = eqObjects;
+
 const shirtObject = { color: "red", size: "medium"};
-const anotherShirtObject = { size: "medium", color: "red"};
+const anotherShirtObject= { size: "medium", color: "red"};
 console.log(eqObjects(shirtObject, anotherShirtObject)); // => true
 
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
 eqObjects(shirtObject , longSleeveShirtObject); // => false
 
 assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
