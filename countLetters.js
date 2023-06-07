46d17af98fb9961425c26bfb9e273c2ab5f0788d
Afreeda <amahesan@uoguelph.ca>
2023-06-07 16:09:50 -0400

    Add module.exports = countLetters

--- countLetters.js
+++ countLetters.js
@@ -24,10 +24,12 @@ const countLetters = function(string) {
   return results;
 };
 
+module.exports = countLetters;
+
 //Test cases:
 const result1 = countLetters('lighthouse in the house');
 console.log(result1);
 
-assertEqual(countLetters('lighthouse in the house')["L"], 2);
-assertEqual(countLetters('lighthouse in the house')["l"], 2);
+assertEqual(countLetters('lighthouse in the house')["h"], 4);
+assertEqual(countLetters('lighthouse in the house')["e"], 3);
 assertEqual(countLetters('LHL is the best!')["b"], 1);
