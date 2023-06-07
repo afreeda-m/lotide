60b841238a1079adb17d0b5018316fe1ede24fef
Afreeda <amahesan@uoguelph.ca>
2023-06-07 13:10:52 -0400

    Delete manual test code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1,array2)) {
@@ -19,8 +8,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 4], [1, 2, 3]);
-assertArraysEqual([1, "2", 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 4], [1, 2, 3]);
-assertArraysEqual([1, 2, 4], [1, "hello", 3]);
+module.exports = assertArraysEqual;
