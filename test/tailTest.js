fc06a7b829866d84d1a0165b961a44458d60e2a7
Afreeda <amahesan@uoguelph.ca>
2023-06-07 14:09:11 -0400

    Add assert.deepEqual function

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,12 +3,12 @@ const tail = require('../tail');
 
 describe("#tail", () => {
   it("returns [2, 3] for [1, 2, 3]", () => {
-    assert(tail([1, 2, 3]), [2, 3]);
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
   });
   it("returns '5' for ['woofwoof', '5']", () => {
-    assert(tail(['woofwoof',5]), [5]);
+    assert.deepEqual(tail(['woofwoof',5]), [5]);
   });
   it("returns [] for []", () => {
-    assert(tail([1]), [1]);
+    assert(tail([]), [1]);
   });
 });
