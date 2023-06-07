245693bca82574086b554ffb8e3dc9f392aebe90
Afreeda <amahesan@uoguelph.ca>
2023-06-07 13:30:52 -0400

    Delete manual test code and move middleTest.js

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1,array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   const length = array.length;
   const middleIndex = Math.floor(length / 2);
@@ -34,19 +13,4 @@ const middle = function(array) {
   }
 };
 
-//Test middle function:
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-
-let equal = eqArrays(["my", "favourite", "item"], ["my", "favourite", "item"]);
-
-assertArraysEqual(['favourite'], middle(["my", "favourite", "item", "teddy"]));
-assertArraysEqual(['item'], middle(["my", "item", "teddy"]));
-
-assertArraysEqual(['favourite', 'item'], middle(["my", "favourite", "item", "teddy"]));
+module.exports = middle;
