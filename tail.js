33c50df3ba48b9d85153e19dca0327d27a74f8de
Afreeda <amahesan@uoguelph.ca>
2023-06-07 16:16:09 -0400

    Remove assertEqual and Add module.exports = tail

--- tail.js
+++ tail.js
@@ -1,29 +1,6 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   const newArr = arr.slice(1, arr.length);
   return newArr;
 };
 
-//Test cases
-//console.log(tail([1]));
-//console.log(tail([]));
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-
-const result = tail(["Hello", "Lighthouse", "Labs", "bootcamp"]);
-console.log(result);
-assertEqual(result.length, 3);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-assertEqual(result[2], "bootcamp");
-
+module.exports = tail;
