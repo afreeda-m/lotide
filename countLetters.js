4f01c1b306e3ae759019449e2e6886395a9429ad
Afreeda <amahesan@uoguelph.ca>
2023-06-10 17:08:14 -0400

    Delete and move test code to countLettersTest.js

--- countLetters.js
+++ countLetters.js
@@ -1,22 +1,19 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// function which counds the number of time a character appears in a string
 const countLetters = function(string) {
+  //initiate an empty object to keep track of each character
   const results = {};
 
+  // loop through the string to get each character
   for (let char of string) {
+    // skip spaces
     if (char === ' ') {
       continue;
     } else {
+      // if character is already in the object, increment count by 1
       if (results[char]) {
         results[char] += 1;
       } else {
+        // else, create a new key for the character and set value to 1
         results[char] = 1;
       }
     }
@@ -26,10 +23,10 @@ const countLetters = function(string) {
 
 module.exports = countLetters;
 
-//Test cases:
-const result1 = countLetters('lighthouse in the house');
-console.log(result1);
+// //Test cases:
+// const result1 = countLetters('lighthouse in the house');
+// console.log(result1);
 
-assertEqual(countLetters('lighthouse in the house')["h"], 4);
-assertEqual(countLetters('lighthouse in the house')["e"], 3);
-assertEqual(countLetters('LHL is the best!')["b"], 1);
+// assertEqual(countLetters('lighthouse in the house')["h"], 4);
+// assertEqual(countLetters('lighthouse in the house')["e"], 3);
+// assertEqual(countLetters('LHL is the best!')["b"], 1);
