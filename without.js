751c45bfa239efad9f882af331e3be7a238f8e7e
Afreeda <amahesan@uoguelph.ca>
2023-06-10 17:13:34 -0400

    Move test code to withoutTest.js file

--- without.js
+++ without.js
@@ -1,24 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1,array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 // Generate a new array without the items that need to be removed
 const without = function(source, itemsToRemove) {
   // if source array is empty, nothing to remove so return undefined
@@ -26,6 +5,7 @@ const without = function(source, itemsToRemove) {
     return undefined;
   }
 
+  // initiate an empty array
   let newArr = [];
   
   //loop through source array to see if any elements are in the itemsToRemove array, if so, ignore, else push element into new array
@@ -34,21 +14,8 @@ const without = function(source, itemsToRemove) {
       newArr.push(source[i]);
     }
   }
+  // return the final array
   return newArr;
 }
 
 module.exports = without;
-
-console.log(without([1, 2, 3],[1]));
-console.log(without(["1", "2", "3"], [1, 2, "3"]));
-console.log(without(["hello", "world", "lighthouse"], ["lighthouse"]));
-
-const words = ["hello", "world", "lighthouse"];
-const arr = without(words, ['world']);
-console.log(arr);
-assertArraysEqual(arr, ["hello", "lighthouse"]);
-
-const moreWordsToTest = ["a", "e", "i", "o", "u", "y"];
-const arrWithout = without(moreWordsToTest, ['a', 'e', 'i', 'o'])
-console.log(arrWithout);
-assertArraysEqual(arrWithout, ["u", "y"]);
