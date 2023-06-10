37e93196fac732cf5c14c860141fe52ca2020d4d
Afreeda <amahesan@uoguelph.ca>
2023-06-10 00:06:36 -0400

    Refactor without function and add comments

--- without.js
+++ without.js
@@ -19,26 +19,23 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
+// Generate a new array without the items that need to be removed
 const without = function(source, itemsToRemove) {
+  // if source array is empty, nothing to remove so return undefined
+  if (source === [] || source === ""){
+    return undefined;
+  }
+
   let newArr = [];
-
-  for (let i = 0; i < source.length; i++) {
-    let found = false;
-
-    for (let y = 0; y < itemsToRemove.length; y++) {
-      if (itemsToRemove[y] === source[i]) {
-        found = true;
-        break;
-      }
-    }
-
-    if (!found) {
+  
+  //loop through source array to see if any elements are in the itemsToRemove array, if so, ignore, else push element into new array
+  for (let i = 0; i < source.length; i++){
+    if (!itemsToRemove.includes(source[i])){
       newArr.push(source[i]);
     }
   }
   return newArr;
-
-};
+}
 
 module.exports = without;
 
