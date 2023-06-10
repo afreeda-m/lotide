b8e6e28942a6758e7173d1e60552eee6e05d0b7b
Afreeda <amahesan@uoguelph.ca>
2023-06-10 16:21:45 -0400

    Delete and move tests to assertObjectsEqualTest.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,22 +1,8 @@
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
-
-  for (let key in object1) {
-    if (object2[key]) {
-      if (object1[key] !== object2[key]) {
-        return false;
-      }
-    } else {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(object1, object2) {
   const inspect = require('util').inspect;
+  // check if objects are equal based on eqObjects function
   if (eqObjects(object1, object2)) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(object1)} === ${inspect(object2)}`);
   } else {
@@ -25,5 +11,3 @@ const assertObjectsEqual = function(object1, object2) {
 };
 
 module.exports = assertObjectsEqual;
-
-assertObjectsEqual({a: '1', b: 2 }, { b: 2, a: '1' });
