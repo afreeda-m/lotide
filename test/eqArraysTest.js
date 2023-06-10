2eea098493966bc20bf7e00d08ad38d49862564b
Afreeda <amahesan@uoguelph.ca>
2023-06-10 09:38:53 -0400

    Add chai assert test for eqArrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,10 +1,17 @@
-const assertEqual = require('../assertEqual');
+const assert= require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["hello", 2, 3], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), false);
-assertEqual(eqArrays([1], [1, 4, "3"]), false);
-assertEqual(eqArrays(["LHL", "bootcamp", "may"], ["LHL", "bootcamp", "may"]), true);
-assertEqual(eqArrays([], []), true);
+describe("#eqArrays", () => {
+  it("returns true for eqArray([1, 2, 3], [1, 2, 3])", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+  })
+  it("returns false for eqArrays([1, 2, 3], [1, 4, 3])", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 4, 3]), false)
+  })
+  it("returns true for eqArrays(['LHL', 'bootcamp', 'may'], ['LHL', 'bootcamp', 'may'])", () => {
+    assert.deepEqual(eqArrays(["LHL", "bootcamp", "may"], ["LHL", "bootcamp", "may"]), true)
+  })
+  it("returns true for eqArrays([], [])", () => {
+    assert.deepEqual(eqArrays([], []), true)
+  })
+});
