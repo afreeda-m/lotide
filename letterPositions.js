e31c5d2e13fc0e8bc5f5035f008747ecc130e643
Afreeda <amahesan@uoguelph.ca>
2023-06-10 17:20:43 -0400

    Delete test code, move to letterPositionsTest.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,36 +1,20 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1,array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const letterPositions = function(sentence) {
+  // initiate an empty object to keep track of index of each character in string
   const results = {};
 
+  // loop through the string to get each character
   for (let i = 0; i < sentence.length; i++) {
     const char = sentence[i];
-
+    
+    // skip spaces
     if (char === ' ') {
       continue;
     } else {
+      // if character is in the results object, push the index number to the existing indices
       if (results[char]) {
         results[char].push(i);
       } else {
+        // else, clear a new key for the char and put the current index as value
         results[char] = [i];
       }
     }
@@ -39,24 +23,3 @@ const letterPositions = function(sentence) {
 };
 
 module.exports = letterPositions;
-
-const result = letterPositions("LHL rocks!");
-console.log(result);
-
-const test1 = letterPositions('hello');
-// h: [0], e: [1], l: [2, 3], o: [4]
-assertArraysEqual(test1['h'], [0]);
-assertArraysEqual(test1['e'], [1]);
-assertArraysEqual(test1['l'], [2, 3]);
-assertArraysEqual(test1['o'], [4]);
-
-const test2 = letterPositions('LHL rocks!');
-// L: [0, 2], H: [1], r: [4], o: [5], c: [6], k: [7], s: [8], '!': [9]
-assertArraysEqual(test2['L'], [0, 2]);
-assertArraysEqual(test2['H'], [1]);
-assertArraysEqual(test2['r'], [4]);
-assertArraysEqual(test2['o'], [5]);
-assertArraysEqual(test2['c'], [6]);
-assertArraysEqual(test2['k'], [7]);
-assertArraysEqual(test2['s'], [8]);
-assertArraysEqual(test2['!'], [9]);
