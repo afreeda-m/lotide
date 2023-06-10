f7a3bffacc23e2eac840edbbc28fab012ae74e95
Afreeda <amahesan@uoguelph.ca>
2023-06-10 16:00:00 -0400

    Delete manual tests and add comments

--- map.js
+++ map.js
@@ -1,27 +1,8 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1,array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
+// modify a given array based on the callback
 const map = function(array, callback) {
   const results = [];
 
+  // loop through the array and send items to callback to be modified and push the return item to a new array
   for (let item of array) {
     results.push(callback(item));
   }
@@ -29,14 +10,3 @@ const map = function(array, callback) {
 };
 
 module.exports = map;
-
-const words = ["ground", "control", "to", "major", "tom"];
-
-const results1 = map(words, word => word[0]);
-const results2 = map(words, word => word[1]);
-const results3 = map(words, word => word.slice(-1));
-console.log(results1);
-
-assertArraysEqual(results1, ['g', 'c', 't', 'm', 't' ]);
-assertArraysEqual(results2, ['r', 'o', 'o', 'a', 'o']);
-assertArraysEqual(results3, ['d', 'l', 'o', 'r', 'm']);
