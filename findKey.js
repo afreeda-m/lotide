0908de0001f2476e823cc4d6b3d64bb73a4eb6e0
Afreeda <amahesan@uoguelph.ca>
2023-06-10 15:44:42 -0400

    Delete manual test and comment code

--- findKey.js
+++ findKey.js
@@ -1,14 +1,7 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// find a key in an object associated with the value given
 const findKey = function(object, callback) {
 
+  // loop through each key value pair and check if it matches the callback
   for (const key in object) {
     if (callback(object[key])) {
       return key;
@@ -17,23 +10,3 @@ const findKey = function(object, callback) {
 };
 
 module.exports = findKey;
-
-console.log(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)); // => "noma"
-
-const test1 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 3);
-
-assertEqual(test1, "Akaleri");
