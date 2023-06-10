418ed820accfdcecfb5199ff31991f00034137a2
Afreeda <amahesan@uoguelph.ca>
2023-06-10 16:46:39 -0400

    Delete and move tests to eqObjectsTest.js file

--- eqObjects.js
+++ eqObjects.js
@@ -1,38 +1,21 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqObjects = function(object1, object2) {
+  // if the number of keys do not match both objects, then objects cannot be equal
   if (Object.keys(object1).length !== Object.keys(object2).length){
     return false;
   }
 
-  for (let key in object1){
-    if (object2[key]){
-      if (object1[key] !== object2[key]){
+  //  if number of keys are equal, then check that the key-value pairs are the same for both objects
+  for (let key in object1) {
+    if (object2[key]) {
+      if (object1[key] !== object2[key]) {
         return false;
       }
     } else {
-      
       return false;
     }
   }
+  // both objects are same if above conditions are false
   return true;
 };
 
 module.exports = eqObjects;
-
-const shirtObject = { color: "red", size: "medium"};
-const anotherShirtObject= { size: "medium", color: "red"};
-console.log(eqObjects(shirtObject, anotherShirtObject)); // => true
-
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-eqObjects(shirtObject , longSleeveShirtObject); // => false
-
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
-assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
