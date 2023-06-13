d0f338e1bc7b6ab3b79e15e1a6867ef86aab5c25
Afreeda <amahesan@uoguelph.ca>
2023-06-13 11:47:38 -0400

    Fixed bug to account for key values can be arrays

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const eqObjects = function(object1, object2) {
   // if the number of keys do not match both objects, then objects cannot be equal
   if (Object.keys(object1).length !== Object.keys(object2).length){
@@ -7,14 +9,19 @@ const eqObjects = function(object1, object2) {
   //  if number of keys are equal, then check that the key-value pairs are the same for both objects
   for (let key in object1) {
     if (object2[key]) {
-      if (object1[key] !== object2[key]) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
         return false;
       }
     } else {
       return false;
     }
   }
-  // both objects are same if above conditions are false
+
+  //both objects are same if above conditions fail
   return true;
 };
 
